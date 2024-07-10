da17e4818cb9e260c8ad6dc080a2a8702e29c0e2
harro114 <maksim20856@gmail.com>
2024-07-10 17:29:25 +0300

    Django project created

--- timeManager/timeAllocator/views.py
+++ timeManager/timeAllocator/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def task_list(requests):
+    return render(requests, "timeAllocator/index.html")
--- timeManager/timeManager/settings.py
+++ timeManager/timeManager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'timeAllocator'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'timeManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'TimeManagerDB',
+        'USER': 'postgres',
+        'PASSWORD': '1148',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
