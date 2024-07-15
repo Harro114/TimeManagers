154559c90a76c67652d1658831e7310f6f3ed440
harro114 <maksim20856@gmail.com>
2024-07-15 18:14:05 +0300

    Подключение css к шаблону. Добавлена директория static внутри приложения timeAllocator в базовые директории для поиска.

--- timeManager/timeManager/settings.py
+++ timeManager/timeManager/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "timeAllocator/static"
+]
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
